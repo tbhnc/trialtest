1651e4f0203b767d4f7e26a2b4a90780a66ff934
Troels Buus Hansen <tbh@netcompany.com>
2018-03-20 08:46:25 +0100

    ny feature

---
+++ flextrial.Core/Controllers/TestController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+using flextrial.Core.Models;
+using Umbraco.Web.Mvc;
+
+namespace flextrial.Core.Controllers
+{
+    public class TestController : SurfaceController
+    {
+        [HttpPost]
+        public ActionResult Submit(TestViewModel model)
+        {
+            //model not valid, do not save, but return current Umbraco page
+            if (!ModelState.IsValid)
+            {
+                //Perhaps you might want to add a custom message to the ViewBag
+                //which will be available on the View when it renders (since we're not 
+                //redirecting)	    	
+                return CurrentUmbracoPage();
+            }
+
+            //if validation passes perform whatever logic
+            //In this sample we keep it empty, but try setting a breakpoint to see what is posted here
+
+            //Perhaps you might want to store some data in TempData which will be available 
+            //in the View after the redirect below. An example might be to show a custom 'submit
+            //successful' message on the View, for example:
+            TempData.Add("CustomMessage", "Your form was successfully submitted at " + DateTime.Now);
+
+            //redirect to current page to clear the form
+            return RedirectToCurrentUmbracoPage();
+
+            //Or redirect to specific page
+            //return RedirectToUmbracoPage(12345)
+        }
+
+
+
+    }
+}
---
+++ flextrial.Core/Models/TestViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace flextrial.Core.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        
+        public string Email { get; set; }
+
+        public string Comment { get; set; }
+    }
+}
